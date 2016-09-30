c8767ac7a57454932ab5e1484230bdd99147d29b
ShivonQ <realitykeeper@gmail.com>
2016-09-30 17:44:10 -0500

    GoodReadsClient makes a request, catches the XML.  It will have to be parsed into a dictionary.

--- book_model.py
+++ book_model.py
@@ -1,2 +1,6 @@
 '''This is where the peewee model for a book will be stored'''
 import peewee
+
+"""Make sure that this object stores the goodreads _id.  We will need it later.
+    https://www.goodreads.com/search/index.xml?q=Ender&key=4ylN8OWi1dhG5Yhq3PQstA
+    that will show you what types of data the server responds with."""
--- goodreads_client.py
+++ goodreads_client.py
@@ -8,7 +8,14 @@ class goodreads_client():
         self.secret_key = secret_key
 
     def search(self, parameter):
+        # We construct a URL to send to The Goodreads API.  In this case it will take our key, and a
+        # search parameter, either a Author, ISBN, or title. in my example file 'test_client.py'
+        # you will see that I send the parameter 'Ender' and get a bucnh of weird HTML tags back.
         base_url = 'https://www.goodreads.com/search/index.xml?q='+parameter+'&key='+secret_key
-        response = requests.get(base_url)
-        print(response.url)
+        # An API call MUST be use .post not .get, because that tells the server we expect something back.
+        response = requests.post(base_url)
+        # TODO: Parse XML to Dictionary so it can be used.
+        print(response.text)
+
+
 
