7aed57cc850eff4c42e3483269842fd38cf5031e
ShivonQ <realitykeeper@gmail.com>
2016-10-01 12:28:07 -0500

    Client is better, has 2 functions now.

--- goodreads_client.py
+++ goodreads_client.py
@@ -19,9 +19,14 @@ class goodreads_client():
         dict_form = ET.fromstring(response.text)
         # This should let me iterate through and get what I want.
         results = parse_best_books(dict_form)
+        return results
 
-
-
+    def all_books_by_author(self, auth_id):
+        base_url = 'https://www.goodreads.com/series/list/'+auth_id+'.xml?key='+secret_key
+        response = requests.post(base_url)
+        dict_form = ET.fromstring(response.text)
+        results = parse_best_books(dict_form)
+        return results
 
 
 
@@ -29,7 +34,7 @@ def parse_best_books(dict_form):
     all_books = []
     for best_book in dict_form.iter('best_book'):
         # Load up blank dictionary to store book data
-        book = {'id': 0, 'title': '', 'author_info': {'auth_id': 0, 'author': ''}, 'image': ''}
+        book = {'id': 0, 'title': '', 'author_info': {'auth_id': 0, 'author': ''}}
 
         '''Due to the structure of the XML parsed by the Element-tree we have to iterate this way to get our data.
            All other methods I tried failed, and this one yields positive results.'''
@@ -41,7 +46,8 @@ def parse_best_books(dict_form):
         for author_info in best_book.findall('author'):
             book['author_info']['auth_id'] = author_info.find('id').text
             book['author_info']['author'] = author_info.find('name').text
-        for image_url in best_book.findall('image_url'):
-            book['image'] = image_url.text
+        #     This part could be reinserted at a later time if image support was a thing
+        # for image_url in best_book.findall('image_url'):
+        #     book['image'] = image_url.text
         all_books.append(book)
     return all_books
